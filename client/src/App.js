62fb29fb0b03d9b7a7235ef87200a28677d79a43
Joseph Jin <jerkjoe61@gmail.com>
2020-02-11 16:17:22 -0800

    reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,43 @@
 import React, { Fragment } from 'react';
-import Navbar from './components/layout/Navbar'
-import Landing from './components/layout/Landing'
-import './App.css'
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Alert from './components/layout/Alert';
+import { Login } from './components/auth/Login';
+import Register from './components/auth/Register';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+
+import store from './store';
+
+import './App.css';
 function App() {
     return (
-        <Fragment>
-            <Navbar />
-            <Landing />
-        </Fragment>
+        <Provider store={store}>
+            <Router>
+                <Fragment>
+                    <Navbar />
+                    <Route exact path="/" component={Landing}></Route>
+
+                    <section className="container">
+                        <Alert></Alert>
+                        <Switch>
+                            <Route
+                                exact
+                                path="/register"
+                                component={Register}
+                            ></Route>
+                            <Route
+                                exact
+                                path="/login"
+                                component={Login}
+                            ></Route>
+                        </Switch>
+                    </section>
+                </Fragment>
+            </Router>
+        </Provider>
     );
 }
 
